8db60e025d2c8ec3879910627a6c84d251a196f4
khakuzz <38999102+khakuzz@users.noreply.github.com>
2021-01-06 16:33:29 +1100

    initial commit

---
+++ training.py
@@ -0,0 +1,13 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+import xmltodict
+import random
+from os import listdir
+from os.path import isfile, join
+import torchvision 
+import torch 
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
